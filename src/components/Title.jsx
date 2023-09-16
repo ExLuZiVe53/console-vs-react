4a59e2cbebfb88f915cb335a3d6d1654cfad7768
ExLuZiVe53 <imstudent27@gmail.com>
2023-09-16 10:38:46 +0200

    children than props.color

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledTitle = styled.h1`
-  color: white;
+  color: ${props => props.color};
 `;
 
-export const Title = children => {
-  return <StyledTitle>{children}</StyledTitle>;
+export const Title = props => {
+  return <StyledTitle {...props}></StyledTitle>;
 };
